14403b2d6c3739f9fc0c1ab17ea2bda94755047f
febrana <95363946+febranaputra@users.noreply.github.com>
2023-11-27 19:37:07 +0800

    Add files via upload

---
+++ sql.php
@@ -0,0 +1,10 @@
+<?php
+
+	$server		= "localhost"; 
+	$user		= "root";
+	$password	= ""; 
+	$database	= "2101020025_febranaputra"; 
+
+	$con = mysqli_connect($server,$user,$password,$database);
+
+?>
